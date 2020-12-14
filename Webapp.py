d05f3962d34f423e5d48217a4f1174604e3b79ca
hungteoh123 <baleko12345@gmail.com>
2020-12-14 16:56:06 +0800

    initial

--- Webapp.py
+++ Webapp.py
@@ -1,14 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
+@app.route('/home')
 def home():
-    return "Hello, World!"
-    
-@app.route("/salvador")
-def salvador():
-    return "Hello, Salvador"
-    
+    return render_template('home.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
